481d6055ed805d0ecb89e185e1bfcdc1c87f0179
Moritz Meier <mam14@tu-clausthal.de>
2022-12-21 00:11:26 +0100

    fix

--- settings.cmake
+++ settings.cmake
@@ -1,1 +1,7 @@
-set(KernelPlatform zynq7000 CACHE STRING "")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/tools/cmake-tool/helpers")
+include(application_settings)
+
+correct_platform_strings()
+
+ApplyCommonSimulationSettings("${KernelSel4Arch}")
+ApplyCommonReleaseVerificationSettings(${RELEASE} ${VERIFICATION})
